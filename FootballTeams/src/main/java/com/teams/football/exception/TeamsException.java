1b062d6964c83416f358c44f0924b47e7a831c5d
henry.opara <henry.opara@HYDPCM266147L>
2023-01-28 18:15:40 +0000

    Exception class setup

--- FootballTeams/src/main/java/com/teams/football/exception/TeamsException.java
+++ FootballTeams/src/main/java/com/teams/football/exception/TeamsException.java
@@ -1,5 +1,9 @@
 package com.teams.football.exception;
 
-public class TeamsException {
+public class TeamsException extends Exception{
+	
+	public TeamsException(String message) {
+		super(message);
+	}
 
 }
