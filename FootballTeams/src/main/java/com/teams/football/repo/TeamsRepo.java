b2ace39db564ecfc8d5e22fc4ddbcd7b70cc634f
henry.opara <henry.opara@HYDPCM266147L>
2023-01-28 17:35:45 +0000

    Set up repository

--- FootballTeams/src/main/java/com/teams/football/repo/TeamsRepo.java
+++ FootballTeams/src/main/java/com/teams/football/repo/TeamsRepo.java
@@ -1,5 +1,11 @@
 package com.teams.football.repo;
 
-public class TeamRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teams.football.entity.Teams;
+
+@Repository
+public interface TeamsRepo extends JpaRepository<Teams, Integer> {
 
 }
