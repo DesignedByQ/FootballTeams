827ccc96f4c81ea4af78e4df16f44ddbec8f0063
henry.opara <henry.opara@HYDPCM266147L.ad.infosys.com>
2023-02-01 16:11:48 +0000

    Before the amigos tutorial

--- FootballTeams/src/main/java/com/teams/football/FootballApplication.java
+++ FootballTeams/src/main/java/com/teams/football/FootballApplication.java
@@ -2,13 +2,25 @@ package com.teams.football;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class FootballApplication {
 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan("com.teams.football.repo.TeamsRepo")//to scan repository files
+//@EntityScan("com.teams.football.entity.Teams")
+//@EnableJpaRepositories("com.teams.football.repo.TeamsRepo", "entityManagerFactory")
+public class FootballApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FootballApplication.class, args);
 	}
--- FootballTeams/src/main/java/com/teams/football/api/ControllerAPI.java
+++ FootballTeams/src/main/java/com/teams/football/api/ControllerAPI.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,6 +29,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api")
 @Validated
+@CrossOrigin
 public class ControllerAPI {
 	
 	
@@ -36,9 +38,9 @@ public class ControllerAPI {
 	
 	
 	@PostMapping(value = "/promotion", consumes = {MediaType.ALL_VALUE})
-	public ResponseEntity<TeamsDTO> addTeams(@Valid @RequestBody TeamsDTO TeamsDTO) {
+	public ResponseEntity<TeamsDTO> addTeams(@Valid @RequestBody TeamsDTO teamsDTO) {
 		
-		return ResponseEntity.status(HttpStatus.CREATED).body(serviceDAOImpl.addTeamsService(TeamsDTO));
+		return ResponseEntity.status(HttpStatus.CREATED).body(serviceDAOImpl.addTeamsService(teamsDTO));
 		
 	}
 	
